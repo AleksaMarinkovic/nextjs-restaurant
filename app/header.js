101b6047c367f71ce1df14cb6337124d4f4b3cb7
Aleksa Marinkovic <aleksa.marinkovic456@gmail.com>
2023-04-08 17:42:39 +0200

    Header changes

--- app/header.js
+++ app/header.js
@@ -13,29 +13,62 @@ async function getHeaderInfo() {
   return data.data;
 }
 
+const dayOpenningHourInfo = (openningHours) => {
+  const today = new Date().getDay();
+  switch (today) {
+    case 0:
+      return openningHours.sunday;
+    case 1:
+      return openningHours.monday;
+    case 2:
+      return openningHours.tuesday;
+    case 3:
+      return openningHours.wednesday;
+    case 4:
+      return openningHours.thursday;
+    case 5:
+      return openningHours.friday;
+    case 6:
+      return openningHours.saturday;
+  }
+};
+
 const Header = async () => {
   const headerInfo = await getHeaderInfo();
   return (
     <header className={styles.header}>
-      <Image
-        alt="Restaurant logo"
-        src={
-          settings.backendUrl + headerInfo.attributes.logo.data.attributes.url
-        }
-        width={250}
-        height={250}
-      ></Image>
-      <nav className={styles.nav}>
-        <Link href="/" className={styles.navItemActive}>
-          Home
-        </Link>
-        <Link href="/contact" className={styles.navItemActive}>
-          Contact
-        </Link>
-        <Link href="/menu" className={styles.navItemActive}>
-          Menu
-        </Link>
-      </nav>
+      <div className={styles.leftSideMenu}>
+        Otvoreno danas:
+        {dayOpenningHourInfo(headerInfo.attributes.openningHours)}
+      </div>
+      <div className={styles.middleMenu}>
+        <div className={styles.logoWrapper}>
+          <Image
+            alt="Restaurant logo"
+            src={
+              settings.backendUrl +
+              headerInfo.attributes.logo.data.attributes.url
+            }
+            width={250}
+            height={250}
+          ></Image>
+        </div>
+
+        <nav className={styles.nav}>
+          <Link href="/" className={styles.navItemNonActive}>
+            Home
+          </Link>
+          <Link href="/contact" className={styles.navItemNonActive}>
+            Contact
+          </Link>
+          <Link href="/menu" className={styles.navItemNonActive}>
+            Menu
+          </Link>
+        </nav>
+      </div>
+      <div className={styles.rightSideMenu}>
+        <div>Choose your language</div>
+      </div>
     </header>
   );
 };
