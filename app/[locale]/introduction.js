47f489f8a404cd066c10506e4ca47def8285aea7
Aleksa <you@example.com>
2023-05-15 13:41:52 +0200

    main page changes, switched to grid, added section for contact/info

--- app/[locale]/introduction.js
+++ app/[locale]/introduction.js
@@ -3,13 +3,49 @@ import styles from "./introduction.module.scss";
 import settings from "./settings";
 import mainImage from "../../public/bar-long.webp";
 import { getTranslations } from "next-intl/server";
-import {Link} from "next-intl";
+import { Link } from "next-intl";
+import QueryString from "qs";
+import { Kaushan_Script } from "next/font/google";
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const kaushan = Kaushan_Script({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "400",
+});
+
+const todaysOpenningHours = (openningHours, t) => {
+  const index = new Date().getDay();
+  switch (index) {
+    case 0:
+      return { day: t("sunday"), hours: openningHours.sunday };
+    case 1:
+      return { day: t("monday"), hours: openningHours.monday };
+    case 2:
+      return { day: t("tuesday"), hours: openningHours.tuesday };
+    case 3:
+      return { day: t("wednesday"), hours: openningHours.wednesday };
+    case 4:
+      return { day: t("thursday:"), hours: openningHours.thursday };
+    case 5:
+      return { day: t("friday"), hours: openningHours.friday };
+    case 6:
+      return { day: t("saturday"), hours: openningHours.saturday };
+  }
+};
 
 async function getRestaurantInfo(locale) {
-  const res = await fetch(
-    `${settings.backendUrl}/api/restaurant?fields[0]=name&fields[1]=description&locale=${locale}`,
-    { next: { revalidate: settings.revalidateTime } }
-  );
+  const query = QueryString.stringify({
+    fields: ["name", "description"],
+    locale: [locale],
+    populate: ["openningHours", "location", "contact"],
+  });
+  const res = await fetch(`${settings.backendUrl}/api/restaurant?${query}`, {
+    next: { revalidate: settings.revalidateTime },
+  });
   const data = await res.json();
   return data.data.attributes;
 }
@@ -18,22 +54,54 @@ export default async function Introduction({ params }) {
   const locale = params.locale;
   const restaurantData = await getRestaurantInfo(locale);
   const t = await getTranslations("Homepage");
+  const t1 = await getTranslations("Contact");
   return (
     <div className={styles.wrapper}>
-      <div className={styles.content}>
-        <div className={styles.imageWrapper}>
+      <div className={styles.background}>
+        <div className={styles.image}>
           <Image
+            className={styles.imageWrapper}
             src={mainImage}
             alt="Bar inside of Pancetta"
-            width={455}
-            height={682}
-            style={{display:'block'}}
+            width={500}
+            height={750}
+            style={{ display: "block" }}
           ></Image>
         </div>
-        <div className={styles.textWrapper}>
-          <h1>{restaurantData.name}</h1>
-          <p>{restaurantData.description}</p>
-          <Link href="/menu"  className={styles.mainButton} locale={locale}>{t("menu").toUpperCase()}</Link>
+        <div className={styles.intro}>
+          <div className={styles.textWrapper}>
+            <div className={styles.introHeader}>
+              <h1 className={kaushan.className}>{restaurantData.name}</h1>
+            </div>
+            <p className={styles.description}>{restaurantData.description}</p>
+            <Link href="/menu" className={styles.mainButton} locale={locale}>
+              {t("menu").toUpperCase()}
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className={styles.info}>
+        <div className={styles.infoSection}>
+          <h2 className={kaushan.className}>{t("workingHours")} :</h2>
+
+          <div>{todaysOpenningHours(restaurantData.openningHours, t1).day}</div>
+          <div>
+            {todaysOpenningHours(restaurantData.openningHours, t1).hours}
+          </div>
+        </div>
+        <div className={styles.infoSection}>
+          <h2 className={kaushan.className}>{t("address")} :</h2>
+
+          <div>
+            {restaurantData.location.street}, {restaurantData.location.city},{" "}
+            {restaurantData.location.country}
+          </div>
+        </div>
+        <div className={styles.infoSection}>
+          <h2 className={kaushan.className}>{t("phone")} :</h2>
+          <div>{restaurantData.contact.email}</div>
+          <div>{restaurantData.contact.telephone}</div>
+          <div>{restaurantData.contact.mobile}</div>
         </div>
       </div>
     </div>
